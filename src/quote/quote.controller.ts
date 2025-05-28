57de04b38e4f9746e1180f5a486d09d8f26ffc87
Pranjal Pandey <pranjalpandey92@gmail.com>
2025-05-29 00:22:01 +0545

    enabled the quote to clients

--- src/quote/quote.controller.ts
+++ src/quote/quote.controller.ts
@@ -2,13 +2,19 @@
 import { Controller, Post, Get, Body, Param } from '@nestjs/common';
 import { QuotesService } from './quote.service';
 import { UserService } from 'src/auth/user.service';
+import { Twilio } from 'twilio';
 
 @Controller('quotes')
 export class QuotesController {
+  private twilioClient: Twilio;
   constructor(
     private readonly quotesService: QuotesService,
-    private readonly userService:  UserService)
-  {}
+    private readonly userService:  UserService,
+  )
+  {
+    this.twilioClient = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    
+  }
 
   @Post('create')
   async createQuote(
@@ -18,19 +24,34 @@ export class QuotesController {
     @Body('message') message: string,
 
   ) {
-    const quotes =  await this.quotesService.createQuote(listingId, agentId, amount, message);
 
+    const customerPhone = '+447887770243'
+
+    const createQuote = await this.quotesService.createQuote(listingId, agentId, amount, message);
     //send sms notification
     const baseUrl = 'https://scrape4you.onrender.com/list-quotes/';
     const localUrl = 'http://localhost:5000/list-quotes/'
-    const quotesLink = `${localUrl}${quotes._id}/${listingId}`;
+    const quotesLink = `${baseUrl}${listingId}/${agentId}`;
     const userMsg = `Hello, you have received a new quote of ${amount}. Click this link to view the details: ${quotesLink}`
 
-    return {
+    if (createQuote){
+      await this.twilioClient.messages.create({
+        to: customerPhone,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        body: userMsg
+      });
+
+      return {
       success: true,
       message: "quote submitted successfully",
       userMsg: userMsg
     }
+
+  }
+
+  else{
+    return{success: false, message: "quote sent failed."}
+  }
 
   }
 
